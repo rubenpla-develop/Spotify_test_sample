d72bf5325ec5d43d21e28751c26893d82f1dc805
Ruben <rubenpla.develop@gmail.com>
2016-05-12 00:48:31 +0200

    Constants and Spotify callbacks basics implemented

--- app/src/main/java/com/spotify/sdk/spotify_test_sample/MainActivity.java
+++ app/src/main/java/com/spotify/sdk/spotify_test_sample/MainActivity.java
@@ -1,13 +1,60 @@
 package com.spotify.sdk.spotify_test_sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.PlayerStateCallback;
+
+public class MainActivity extends AppCompatActivity implements PlayerNotificationCallback, ConnectionStateCallback{
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private Player mPlayer;
+    private PlayerStateCallback mPLayerStateCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d(TAG, "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("TAG", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+        Log.d("TAG", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("TAG", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("TAG", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("TAG", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+        Log.d("TAG", "Playback error received: " + errorType.name());
     }
 }
--- app/src/main/java/utils/Constants.java
+++ app/src/main/java/utils/Constants.java
@@ -4,4 +4,8 @@ package utils;
  * Created by RubenPla on 12/05/2016.
  */
 public class Constants {
+
+    public static final String CLIENT_ID =  "da0287843edb437a9c101e624eec97b6";
+    public static final String CLIENT_SECRET = "3b2641c7255342f0903ec28401f2e187";
+    public static final String REDIRECT_URI = "rpladevelop-spotisdk-sample-callback://callback";
 }
