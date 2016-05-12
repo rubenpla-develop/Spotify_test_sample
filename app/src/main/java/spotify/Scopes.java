afa2cc0fe7da33a4182b68a9382df76e0f9353a6
Ruben <rubenpla.develop@gmail.com>
2016-05-12 02:02:18 +0200

    Spotify Scopes Class created

---
+++ app/src/main/java/spotify/Scopes.java
@@ -0,0 +1,13 @@
+package spotify;
+
+/**
+ * Created by RubenPla on 12/05/2016.
+ */
+public class Scopes {
+
+    public static final String USER_READ_PRIVATE = "user-read_private";
+    public static final String STREAMING = "streaming";
+    public static final String PLAYLIST_READ_PRIVATE = "playlist_read_private";
+
+
+}
